ca6091f7a9db506a5d9a33a920b564e0f8b61445
Mário <cardozo27cv@gmail.com>
2020-08-12 23:36:07 -0100

    update 11/08/2020

--- js/excelJson.js
+++ js/excelJson.js
@@ -1,19 +1,35 @@
 
 
-function ProcessExcel(data) {
 
-    
-    //Read the Excel File data.
-    var workbook = XLSX.read(data, {
-        type: 'binary'
-    });
+ var ExcelToJSON = function() {
 
-    //Fetch the name of First Sheet.
-    var firstSheet = workbook.SheetNames[0];
+    this.parseExcel = function(file) {
+      var reader = new FileReader();
 
-    //Read all rows from First Sheet into an JSON array.
-    var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[firstSheet]);}
+      reader.onload = function(e) {
+        var data = e.target.result;
+        var workbook = XLSX.read(data, {
+          type: 'binary'
+        });
+        workbook.SheetNames.forEach(function(sheetName) {
+          // Here is your object
+          var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+          var json_object = JSON.stringify(XL_row_object);
+          console.log(JSON.parse(json_object));
+          jQuery( '#xlx_json' ).val( json_object );
+        })
+      };
 
-//    var jsonData= ProcessExcel("./assets/candidaturas.xlsx");
+      reader.onerror = function(ex) {
+        console.log(ex);
+      };
 
-    console.log(jsonData);
+      reader.readAsBinaryString(file);
+    };
+};
+
+
+    let data="./candidaturas.xlsx"
+
+  var xl2json = new ExcelToJSON();
+  xl2json.parseExcel(data);
