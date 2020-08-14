61138a8dad6fcd2228eae69bd498544a4990857c
Mário <cardozo27cv@gmail.com>
2020-08-14 22:37:51 -0100

    update

--- js/jsonTotable.js
+++ js/jsonTotable.js
@@ -1,20 +1,19 @@
 function json2table(json, classes) {
-  // CREATE TABLE BODY .
-  var tBody = document.createElement("tbody");	
-			
-  // ADD COLUMN HEADER TO ROW OF TABLE HEAD.
-  for (var i = 0; i < noOfContacts; i++) {
+  let data = JSON.parse(json);
+  console.log(data)
+  let table = document.getElementById(classes);
+  let row, cell;
   
-      var bRow = document.createElement("tr"); // CREATE ROW FOR EACH RECORD .
-      
-      
-      for (var j = 0; j < col.length; j++) {
-        var td = document.createElement("td");
-        td.innerHTML = myContacts[i][col[j]];
-        bRow.appendChild(td);
-      }
-      tBody.appendChild(bRow)
-
+  for (let i = 0; i < data.length; i++) {
+    row = table.insertRow();
+    cell = row.insertCell();
+    cell.textContent = data[i].name;
+    cell = row.insertCell();
+    cell.textContent = data[i].isbn;
+    cell = row.insertCell();
+    cell.textContent = data[i].availableCopies;
   }
-  table.appendChild(tBody);	
+
 }
+
+json2table("./data.json","ttbody")
