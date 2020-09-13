5e1a4ff3894053d78a54af61958a76384b1e5e96
Mário <cardozo27cv@gmail.com>
2020-09-13 22:00:52 -0100

    update sw

--- js/scriptF.js
+++ js/scriptF.js
@@ -127,6 +127,6 @@ function resetDisplay() {
   window.addEventListener("load", function () {
     navigator.serviceWorker
       .register("/serviceWorker.js")
-      .then((e) => {})
-      .catch((e) => {});
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
   });
